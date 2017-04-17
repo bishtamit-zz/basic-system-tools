ee2fca5677459c2f0f002b09e81d18036749cbdb
Amit Bisht <bishtamit54@gmail.com>
2017-04-17 12:11:45 +0530

    Update rename.py
    Added list directory with type

--- rename.py
+++ rename.py
@@ -1,8 +1,8 @@
 import os
-import sys
 
 print "(Linux Only)\n"
 print '''This tool is used to bach rename your files. Please enter correct option:
+0. Print the files and directory in current directory'
 1. Get current working directory
 2. Change directory
 3. Rename single file
@@ -10,12 +10,19 @@ print '''This tool is used to bach rename your files. Please enter correct optio
 5. Exit Anytime
 '''
 
-
 while 1:
     choice = int(raw_input("Enter your choice: "))
     cur_dir = os.getcwd() 
     
-    if choice == 1: # Print current working directory
+    if choice == 0: # print all files and directories in current path
+        for blob in os.listdir(cur_dir):
+            if os.path.isdir(blob):
+                print "d -- {}".format(blob)
+            elif os.path.isfile(blob):
+                print "f -- {}".format(blob)
+            else:
+                print "o -- {}".format(blob)
+    elif choice == 1: # Print current working directory
         print "You are in: {}".format(cur_dir)
     elif choice == 2: # Change to diffrent directory
         new_path = raw_input("Enter the path (either absolute or relative): ")
@@ -29,7 +36,7 @@ while 1:
         except OSError:
             raise
             print "Wrong Path! Try Again."
-    elif choice == 3:
+    elif choice == 3: # Renaming single file
         old_name = raw_input("Please enter the file of directory name: ")
         new_name = raw_input("Please enter the new name: ")
         try:
@@ -38,7 +45,7 @@ while 1:
             raise
             print "File does not exist, try again."            
     elif choice == 4:
-        pass
+        print "This option will be available soon"
     elif choice == 5:
         break
     else:
